9160064faf8cc4206a229eff1d8a0b69371f4839
Stephen-ODriscoll <stephen.odriscoll3@mycit.ie>
2019-11-18 15:51:52 +0000

    set up pom.xml and added DataLoader

---
+++ Assignment2/reverseAuction/src/main/java/ie/stephen/DataLoader.java
@@ -0,0 +1,12 @@
+package ie.stephen;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class DataLoader implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        
+    }
+}
---
+++ Assignment2/reverseAuction/src/main/java/ie/stephen/Main.java
@@ -0,0 +1,12 @@
+package ie.stephen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
